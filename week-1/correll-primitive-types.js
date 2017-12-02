71bf003ce402b16761b9ae388238050bb195d8d7
Ashlee Correll <a.rae.correll@gmail.com>
2017-12-02 11:59:46 -0600

    added primitive type file.

---
+++ week-1/correll-primitive-types.js
@@ -0,0 +1,28 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Ashlee";
+var lastName = "Correll";
+var age = "25";
+var street = "4005 Raynor Parkway";
+var city = "Bellevue";
+var state = "Nebraska";
+var zipCode = "68123";
+
+// output 
+console.log('\n'); 
+
+
+// end program
