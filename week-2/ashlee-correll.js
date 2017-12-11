76ce2429fa2d4251f741bf8469f5e4a475a838d6
Ashlee Correll <a.rae.correll@gmail.com>
2017-12-11 13:43:41 -0600

    modified week 2 folder

--- week-2/ashlee-correll.js
+++ week-2/ashlee-correll.js
@@ -1,6 +1,41 @@
 var header = require('../header.js');
 
-console.log("\nTesting the header.js file's output..."); 
 
-console.log(header.display("Richard", "Krasso", "Exercise 2.1")); 
+
+console.log(header.display("Ashlee", "Correll", "Exercise 2.1")); 
 console.log("\n");
+
+var header = require('../header.js'); 
+
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Exercise 2.3
+    Today's Date
+
+    Hello FirstName LastName!
+
+    Hint: Use your personal header display function and
+    refer to page 178 for implementing function properties
+*/ 
+
+// function properties go here...
+myName.ashleeCorrell = "Ashlee Correll";
+
+// function 
+function myName() {
+    return myName.ashleeCorrell
+    
+}
+
+// output 
+console.log(myName()); 
+
+
+
+
+
+// end program
