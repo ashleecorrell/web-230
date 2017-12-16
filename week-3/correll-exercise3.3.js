e2391e321e40a1a8b66a3cb929b7cd96916fee35
Ashlee Correll <a.rae.correll@gmail.com>
2017-12-16 09:16:22 -0600

    renamed files

---
+++ week-3/correll-exercise3.3.js
@@ -0,0 +1,38 @@
+var header = require('../header.js'); 
+
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Exercise 3.3
+    Today's Date
+
+    // Expected output
+    The enter key was pressed 
+
+*/ 
+
+// Multiway branch of if statements (replace with a switch statement)
+var eventKeyCode = 13;
+
+switch (eventKeyCode) {
+    case eventKeyCode = 13:
+        console.log("The enter key was pressed");
+        break; 
+    case eventKeyCode = 16:
+        console.log("The shift key was pressed"); 
+        break;
+    case eventKeyCode = 32:
+        console.log("The spacebar key was pressed");
+        break;
+    case eventKeyCode = 8:
+        console.log("The backspace / delete key was pressed");
+        break;
+    default:
+        console.log("Unrecognized key press"); 
+}
+
+
+// end program 
