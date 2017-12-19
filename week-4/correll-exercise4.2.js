4682f7a5465a5b6e7928d91852badc882cc414f7
Ashlee Correll <a.rae.correll@gmail.com>
2017-12-19 11:01:58 -0600

    Added attribution to assignment 4.2

--- week-4/correll-exercise4.2.js
+++ week-4/correll-exercise4.2.js
@@ -1,3 +1,14 @@
+/*
+Title: Exercise 4.2
+
+Author: Richard Krasso
+
+Date: 19 December 2017
+
+Modified by: Ashlee Correll
+
+Description: JavaScript coding assignment to learn about arrays
+
 var header = require('../header.js');
 console.log(header.display("Ashlee", "Correll", "Exercise 4.2")); 
 console.log("\n");
@@ -20,7 +31,7 @@ console.log("\n");
 //create an array with 5 fruit elements
 var fruit = ["Banana", "Orange", "Pear", "Apple", "Mango"];
 
-// function
+// function getFruit() that accepts a one dimentional array
 function getFruit(arr) {
     for (var f = 0; f < fruit.length; f++) {
         console.log(fruit[f]);
